aa9f2758cdf5d141b5c97c330d911274dc69b97b
Camilo Romero <camiloaromero23@gmail.com>
2022-12-22 19:32:32 -0500

    Include header, tailwind & svelte

---
+++ src/components/ThemeToggleButton.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import DarkThemeIcon from "./DarkThemeIcon.svelte";
+  import LightThemeIcon from "./LightThemeIcon.svelte";
+
+  let theme = localStorage.getItem("theme") ?? "dark";
+
+  const handleClick = () => {
+    theme = theme === "dark" ? "light" : "dark";
+  };
+
+  $: {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }
+</script>
+
+<button class="p-1 hover:bg-gray-400 active:bg-gray-500 rounded-full w-min h-min" on:click={handleClick}>
+  {#if theme === "dark"}
+    <LightThemeIcon />
+  {:else}
+    <DarkThemeIcon />
+  {/if}
+</button>
+<!-- <img src="/darkThemeIcon.svg" alt="dark mode" /> -->
